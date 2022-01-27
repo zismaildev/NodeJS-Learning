e2529e65f074892b9c9ffc0f78842b30b4124f2b
nattapong <nattapong130247@gmail.com>
2022-01-27 22:18:33 +0700

    Html

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 const port = 3000;
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req, res) => {
     res.send('Hello Zismail')
